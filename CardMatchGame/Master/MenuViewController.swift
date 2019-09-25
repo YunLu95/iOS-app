fb5588b9df884be00ee77e997bd501c4bc552241
YunLu95 <lylpfe@icloud.com>
2019-09-24 20:44:04 -0500

    First Commit

---
+++ CardMatchGame/Master/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  CardMatchGame
+//
+//  Created by panxingyang on 9/24/19.
+//  Copyright © 2019 Yun Lu. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
