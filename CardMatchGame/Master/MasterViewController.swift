19d3a68c971a12f7c8e625476a41afbcfaf15e7d
YunLu95 <lylpfe@icloud.com>
2019-09-27 23:18:06 -0500

    show emoji label on card.

--- CardMatchGame/Master/MasterViewController.swift
+++ CardMatchGame/Master/MasterViewController.swift
@@ -8,13 +8,113 @@
 
 import UIKit
 
-class MasterViewController: UIViewController {
-
+class MasterViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate , UICollectionViewDelegateFlowLayout {
+    let sizeOfGrid = 4
+    let emojiChoices=["👽","😈","🤡","👿","🤢","🎃","😺","💩","👾","🤠","👺","👹","🤖","🌛","🌜","🌟","⛄️","✨"]
+    private let spacing : CGFloat = 10.0
+   // private let numberOfCardPerRow
+    var model = CardModel()
+    var cardArray = [Card]()
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        menuBtn.addTarget(self, action: #selector(menuBtnPressed), for: UIControl.Event.touchUpInside)
+        cardArray = model.getCards()
+        print()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        
+       
     }
+//    func addMenuScreen(){
+//
+//        let storyboard = UIStoryboard(name: "Menu", bundle: nil)
+//        guard let controller = storyboard.instantiateInitialViewController() as? MenuViewController else { return }
+//        controller.willMove(toParent: self)
+//        addChild(controller)
+//        self.view.addSubview(controller.view)
+//        controller.didMove(toParent: self)
+//
+//
+//    }
+    
+    
 
+    func transitionToViewController (){
+        let controller = MenuViewController()
+        controller.willMove(toParent: self)
+        let current = children.first
+        addChild(controller)
+        self.view.addSubview(controller.view)
+        controller.didMove(toParent: self)
+        
+        current?.willMove(toParent: nil)
+        current?.view.removeFromSuperview()
+        current?.didMove(toParent: nil)
+    
+    }
+    
+    
+    @IBAction func menuBtnPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "menuSegue", sender: self)
+        print("Menu pressed!")
 
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        let numberOfCards = sizeOfGrid * sizeOfGrid
+        return numberOfCards
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardCell", for: indexPath) as! CollectionViewCell
+
+        cell.setCard(cardArray[indexPath.row], indexPath)
+        
+        cell.cardLabel.text = cardArray[indexPath.row].cardLabel
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! CollectionViewCell
+        
+        let card = cardArray[indexPath.row]
+        print(card.cardLabel)
+        if card.isFlipped == false {
+            //flip card
+            cell.flip()
+            card.isFlipped = true
+        }else{
+            cell.filpBack()
+            card.isFlipped = false
+        }
+        
+        
+    }
+    
+    
+    // function I tried to use to get cells auto sized but not working yet.
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfCardPerRow: CGFloat = CGFloat(sizeOfGrid);
+        let spacingBetweenCells: CGFloat = 10;
+        let totalSpacing = (2 * self.spacing) + ( (numberOfCardPerRow - 1) * spacingBetweenCells )
+        
+        if let collection = self.collectionView{
+            let width = (collection.bounds.width - totalSpacing ) / numberOfCardPerRow
+            return CGSize(width: width, height: width)
+        }else{
+            return CGSize(width: 0, height: 0)
+        }
+    }
+    
 }
+
 
--- CardMatchGame/Master/MenuViewController.swift
+++ CardMatchGame/Master/MenuViewController.swift
@@ -7,35 +7,42 @@
 //
 
 import UIKit
-
 class MenuViewController: UIViewController {
-
-//    @IBOutlet var MenuViewController: UIView!
-    @IBOutlet var MenuView: UIView!
     
+    @IBOutlet var MenuView: UIView!
     @IBOutlet weak var menuName: UITextField!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button4: UIButton!
     @IBOutlet weak var button6: UIButton!
-    
+   // @IBOutlet weak var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.addSubview(MenuView)
+        //view.addSubview(MenuView)
         // Do any additional setup after loading the view.
-        
-        
     }
     
-
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func button2pressed(_ sender: UIButton){
+        //sizeOfGrid = 2;
+        print("2 pressed!")
     }
     
+    @IBAction func button4pressed(_ sender: UIButton){
+        print("4 pressed!")
 
+    }
+    
+    @IBAction func button6pressed(_ sender: UIButton){
+        print("6 pressed!")
+
+    }
+    
+//    @IBAction func doneButtonPressed(_ sender: UIButton) {
+//        performSegue(withIdentifier: "masterSegue", sender: self)
+//        print("Done Button Pressed!")
+//    }
+//
+}
+extension MenuViewController: UITextFieldDelegate{
+    
 }
